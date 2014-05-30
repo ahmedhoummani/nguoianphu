c3c39f48551afb56182b6a59526071b9ed903bd7
nguoianphu <nguoianphu@gmail.com>
2014-05-30 15:59:48 +0700

    change ducks faces to left-right add jump

--- duck/js/main.js
+++ duck/js/main.js
@@ -6,18 +6,18 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser_example', {
 });
 
 function preload() {
-    
+
     //  This sets a limit on the up-scale
-//    game.scale.maxWidth = 800;
-//    game.scale.maxHeight = 600;
+    //    game.scale.maxWidth = 800;
+    //    game.scale.maxHeight = 600;
 
     //  Then we tell Phaser that we want it to scale up to whatever the browser can handle, but to do it proportionally
-//    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-//    game.scale.setScreenSize();
-    
+    //    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    //    game.scale.setScreenSize();
+
     game.load.image('duck', 'assets/duck/duck-r50.png');
     //    game.load.image('shadow', 'assets/duck/shadow.png');
-    game.load.spritesheet('ducks', 'assets/duck/ducks.png', 60, 50, 4);
+    game.load.spritesheet('ducks', 'assets/duck/ducks1.png', 59, 50);
     game.load.audio('jump', 'assets/audio/jump.wav');
 }
 
@@ -38,10 +38,10 @@ function create() {
     ducks.anchor.setTo(0.5, 0.5);
 
     //  Our two animations, walking left and right.
-    ducks.animations.add('left', [0], 6, true);
-    ducks.animations.add('right', [1], 6, true);
-    ducks.animations.add('face', [2], 6, true);
-    ducks.animations.add('back', [3], 6, true);
+    ducks.animations.add('left', [0], 5, true);
+    ducks.animations.add('right', [1], 5, true);
+    //    ducks.animations.add('face', [2], 5, true);
+    //    ducks.animations.add('back', [3], 5, true);
 
     //  This will force it to decelerate and limit its speed
     game.physics.enable(ducks, Phaser.Physics.ARCADE);
@@ -63,7 +63,7 @@ function create() {
     game.physics.enable(duck, Phaser.Physics.ARCADE);
 
     //	Tell it we don't want physics to manage the rotation
-    duck.body.allowRotation = false;
+    ducks.body.allowRotation = false;
 
 
     //  Our controls.
@@ -71,19 +71,11 @@ function create() {
 
     game.input.onDown.add(jump, this);
 
-
-
-
 }
 
 function update() {
 
-    game.physics.arcade.moveToPointer(duck, 60, game.input.activePointer, 500);
-
-    if (duck.angle < 0) {
-        duck.angle = 0;
-    }
-
+    game.physics.arcade.moveToPointer(ducks, 100, game.input.activePointer, 1000);
 
     // ducks faces
 
@@ -91,46 +83,78 @@ function update() {
 
         ducks.animations.play('right');
 
+        if (ducks.angle > 0) {
+            ducks.angle = 0;
+        }
+
     }
     if (ducks.x > game.input.x) {
 
         ducks.animations.play('left');
 
-    }
-    if (ducks.y < game.input.y) {
+        if (ducks.angle < 0) {
+            ducks.angle = 0;
+        }
 
-        ducks.animations.play('face');
     }
+    //    if (ducks.y < game.input.y) {
+    //
+    //        ducks.animations.play('face');
+    //    }
 
-    if (ducks.y > game.input.y) {
-
-        ducks.animations.play('back');
-    }
+    //    if (ducks.y > game.input.y) {
+    //
+    //        ducks.animations.play('back');
+    //    }
 
 }
 
 function render() {
 
-    game.debug.spriteInfo(ducks, 32, 32);
-    game.debug.spriteInfo(duck, 32, 100);
+    //    game.debug.spriteInfo(ducks, 32, 32);
+    //    game.debug.text(tan_right, 32, game.world.centerY);
+    //    game.debug.spriteInfo(duck, 32, 100);
     //    game.debug.inputInfo(32, 42);
 
 }
 
 function jump() {
 
+    // Add a vertical velocity to the bird
+    ducks.body.velocity.y = -1000;
+
     // create an animation on the bird
-    var animation = game.add.tween(duck);
+    var animation = game.add.tween(ducks);
 
     // Set the animation to change the angle of the sprite to -20° in 100 milliseconds
-    animation.to({
-        angle: -45
-    }, 100);
+    // ducks faces
+
+    if (ducks.x < game.input.x) {
+
+        ducks.animations.play('right');
+
+        animation.to({
+            angle: 50
+        }, 150);
+
+    }
+    if (ducks.x > game.input.x) {
+
+        ducks.animations.play('left');
+
+        animation.to({
+            angle: -50
+        }, 150);
+
+    }
+
 
     // And start the animation
     animation.start();
 
     jump_sound.play();
+
+
 
 
 }
