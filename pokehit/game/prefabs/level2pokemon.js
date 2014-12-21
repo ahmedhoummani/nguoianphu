f9b7a8fd146f8fac5c42a4dc25e70671ce49f8e3
nguoianphu <nguoianphu@gmail.com>
2014-12-21 13:48:12 +0700

    add dewgong edit levels

--- pokehit/game/prefabs/level2pokemon.js
+++ pokehit/game/prefabs/level2pokemon.js
@@ -8,22 +8,39 @@ var Level2pokemon = function(a) {
 
 	switch (this._levelNumber) {
 		case 1:
-		case 8:
-		case 15:
-		case 22 :
 			pokemon = 'weedle', pokemon_name = 'Weedle', pokemon_type = 'grass', pokemon_icon = 'weedle_icon.png', frame_left = [
 					'01.png', '02.png', '03.png'], frame_ghostleft = ['07.png',
 					'08.png', '09.png'], frame_right = ['04.png', '05.png',
 					'06.png'], frame_ghostright = ['10.png', '11.png', '12.png'];
 			break;
+		case 10:
+			pokemon = 'weedle', pokemon_name = 'Weedle', pokemon_type = 'grass', pokemon_icon = 'weedle_icon.png', frame_ghostleft = [
+					'01.png', '02.png', '03.png'], frame_left = ['07.png',
+					'08.png', '09.png'], frame_ghostright = ['04.png', '05.png',
+					'06.png'], frame_right = ['10.png', '11.png', '12.png'];
+			break;
 		case 2:
+			pokemon = 'goldfish', pokemon_name = 'Goldfish', pokemon_type = 'water', pokemon_icon = 'goldfish_icon.png', frame_ghostleft  = [
+					'01.png', '02.png', '02.png', '03.png', '04.png', '04.png', '05.png', '05.png', '06.png',
+					'07.png', '08.png'], frame_left = ['17.png', '18.png'], frame_ghostright = ['09.png', '10.png', '11.png', '11.png', '12.png', '12.png', '13.png', '14.png', '14.png', '15.png', '16.png'], frame_right = ['19.png', '20.png'];
+			break;
+		case 11:
+		case 19:
 			pokemon = 'goldfish', pokemon_name = 'Goldfish', pokemon_type = 'water', pokemon_icon = 'goldfish_icon.png', frame_left = [
 					'01.png', '02.png', '02.png', '03.png', '04.png', '04.png', '05.png', '05.png', '06.png',
 					'07.png', '08.png'], frame_ghostleft = ['17.png', '18.png'], frame_right = ['09.png', '10.png', '11.png', '11.png', '12.png', '12.png', '13.png', '14.png', '14.png', '15.png', '16.png'], frame_ghostright = ['19.png', '20.png'];
 			break;
-		case 9:
-		case 16:
-		case 23 :
+		case 3:
+		case 20:
+			pokemon = 'muk', pokemon_name = 'Grimer', pokemon_type = 'sand', pokemon_icon = 'muk_icon.png', frame_ghostleft = [
+					'01.png', '02.png', '03.png', '04.png', '04.png'], frame_left = [
+					'09.png', '09.png', '10.png', '10.png'], frame_ghostright = [
+					'05.png', '06.png',
+					'07.png', '08.png', '08.png'], frame_right = [
+					'11.png', '11.png', '12.png', '12.png'];
+			break;
+		case 12:
+		case 25:
 			pokemon = 'muk', pokemon_name = 'Grimer', pokemon_type = 'sand', pokemon_icon = 'muk_icon.png', frame_left = [
 					'01.png', '02.png', '03.png', '04.png', '04.png'], frame_ghostleft = [
 					'09.png', '09.png', '10.png', '10.png'], frame_right = [
@@ -31,47 +48,72 @@ var Level2pokemon = function(a) {
 					'07.png', '08.png', '08.png'], frame_ghostright = [
 					'11.png', '11.png', '12.png', '12.png'];
 			break;
-		case 3
-		:case 10:
-		case 17:
-		case 24 :
+		case 4 :
+		case 21:
 			pokemon = 'beedrill', pokemon_name = 'Beedrill', pokemon_type = 'grass', pokemon_icon = 'beedrill_icon.png', frame_left = [
 					'01.png', '02.png', '03.png', '04.png', '05.png'], frame_ghostleft = [
 					'11.png','11.png','12.png','12.png'], frame_right = [
 					'06.png','07.png', '08.png', '09.png', '10.png'], frame_ghostright = [
 					'11.png','11.png','12.png','12.png'];
 			break;
-		case 4:
-		case 11:
-		case 18:
-		case 25 :
+		case 13:
+		case 26:
+			pokemon = 'beedrill', pokemon_name = 'Beedrill', pokemon_type = 'grass', pokemon_icon = 'beedrill_icon.png', frame_ghostleft = [
+					'01.png', '02.png', '03.png', '04.png', '05.png'], frame_left = [
+					'11.png','11.png','12.png','12.png'], frame_ghostright = [
+					'06.png','07.png', '08.png', '09.png', '10.png'], frame_right = [
+					'11.png','11.png','12.png','12.png'];
+			break;		
+		case 5:
+		case 14:
+			pokemon = 'dewgong', pokemon_name = 'Dewgong', pokemon_type = 'water', pokemon_icon = 'dewgong_icon.png', frame_left = [
+					'01.png', '02.png', '03.png', '04.png', '05.png', '06.png'], frame_ghostleft = [
+					'01.png'], frame_right = [
+					'07.png', '08.png', '09.png', '10.png', '11.png', '12.png'], frame_ghostright = [
+					'07.png'];
+			break;			
+		case 6:	
+		case 23:
 			pokemon = 'arcanine', pokemon_name = 'Arcanine', pokemon_type = 'sand', pokemon_icon = 'arcanine_icon.png', frame_left = [
 					'01.png', '01.png', '02.png', '02.png'], frame_ghostleft = [
 					'05.png', '05.png', '06.png', '06.png'], frame_right = [
 					'03.png', '03.png', '04.png', '04.png'], frame_ghostright = [
 					'07.png', '07.png', '08.png', '08.png'];
 			break;
-		case 5:
-		case 12:
-		case 19:
-		case 26 :
+		case 15:
+			pokemon = 'arcanine', pokemon_name = 'Arcanine', pokemon_type = 'sand', pokemon_icon = 'arcanine_icon.png', frame_ghostleft = [
+					'01.png', '01.png', '02.png', '02.png'], frame_left = [
+					'05.png', '05.png', '06.png', '06.png'], frame_ghostright = [
+					'03.png', '03.png', '04.png', '04.png'], frame_right = [
+					'07.png', '07.png', '08.png', '08.png'];
+			break;
+		case 7:
+		case 24:
 			pokemon = 'steelix', pokemon_name = 'Steelix', pokemon_type = 'sand', pokemon_icon = 'steelix_icon.png', frame_left = [
 					'01.png', '02.png', '03.png', '04.png', '05.png', '06.png',
 					'07.png', '08.png', '09.png'], frame_ghostleft = ['19.png', '20.png', '21.png'], frame_right = ['10.png', '11.png', '12.png', '13.png', '14.png', '15.png', '16.png', '17.png', '18.png'], frame_ghostright = ['22.png', '23.png', '24.png'];			
 			break;
-		case 6:
-		case 13:
-		case 20:
+		case 16:
+			pokemon = 'steelix', pokemon_name = 'Steelix', pokemon_type = 'sand', pokemon_icon = 'steelix_icon.png',frame_ghostleft  = [
+					'01.png', '02.png', '03.png', '04.png', '05.png', '06.png',
+					'07.png', '08.png', '09.png'], frame_left = ['19.png', '20.png', '21.png'], frame_ghostright = ['10.png', '11.png', '12.png', '13.png', '14.png', '15.png', '16.png', '17.png', '18.png'], frame_right = ['22.png', '23.png', '24.png'];			
+			break;
+		case 8:
 		case 27:
 			pokemon = 'gyarados', pokemon_name = 'Gyarados', pokemon_type = 'water', pokemon_icon = 'gyarados_icon.png', frame_left = [
 					'01.png', '02.png', '03.png', '04.png', '05.png', '06.png',
 					'07.png', '08.png', '09.png', '10.png', '11.png', '12.png'], frame_ghostleft = ['25.png', '25.png', '26.png', '26.png'], frame_right = ['13.png', '14.png', '15.png', '16.png', '17.png', '18.png', '19.png', '20.png', '21.png', '22.png', '23.png', '24.png'], frame_ghostright = [
 					'27.png', '27.png', '28.png', '28.png'];			
 			break;
-		case 7:
-		case 14:
-		case 21:
-		case 28 :
+		case 17:
+			pokemon = 'gyarados', pokemon_name = 'Gyarados', pokemon_type = 'water', pokemon_icon = 'gyarados_icon.png', frame_ghostleft = [
+					'01.png', '02.png', '03.png', '04.png', '05.png', '06.png',
+					'07.png', '08.png', '09.png', '10.png', '11.png', '12.png'], frame_left = ['25.png', '25.png', '26.png', '26.png'], frame_ghostright = ['13.png', '14.png', '15.png', '16.png', '17.png', '18.png', '19.png', '20.png', '21.png', '22.png', '23.png', '24.png'], frame_right = [
+					'27.png', '27.png', '28.png', '28.png'];
+			break;
+		case 9:
+		case 22:
+		case 28:
 			pokemon = 'charizard', pokemon_name = 'Charizard', pokemon_type = 'sand', pokemon_icon = 'charizard_icon.png', frame_left = [
 					'01.png', '02.png', '03.png', '04.png', '05.png', '06.png',
 					'07.png', '08.png'], frame_ghostleft = ['17.png', '18.png',
@@ -79,6 +121,14 @@ var Level2pokemon = function(a) {
 					'12.png', '13.png', '14.png', '15.png', '16.png'], frame_ghostright = [
 					'17.png', '18.png', '19.png'];			
 			break;
+		case 18:
+			pokemon = 'charizard', pokemon_name = 'Charizard', pokemon_type = 'sand', pokemon_icon = 'charizard_icon.png', frame_ghostleft = [
+					'01.png', '02.png', '03.png', '04.png', '05.png', '06.png',
+					'07.png', '08.png'], frame_left = ['17.png', '18.png',
+					'19.png'], frame_ghostright = ['09.png', '010.png', '11.png',
+					'12.png', '13.png', '14.png', '15.png', '16.png'], frame_right = [
+					'17.png', '18.png', '19.png'];			
+			break;		
 		default :
 			pokemon = 'weedle', pokemon_name = 'Weedle', pokemon_type = 'grass', pokemon_icon = 'weedle_icon.png', frame_left = [
 					'01.png', '02.png', '03.png'], frame_ghostleft = ['07.png',
