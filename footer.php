e3da820fe3f1d9163d803bc7980e5a61dab7f5be
nguoianphu <nguoianphu@gmail.com>
2014-12-21 19:26:29 +0700

    statcounter

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php ?>
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=9866399; 
+var sc_invisible=1; 
+var sc_security="f8eb78bd"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="web counter"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/9866399/0/f8eb78bd/1/"
+alt="web counter"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
